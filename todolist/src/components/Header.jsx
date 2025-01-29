a4e9a66fffc2ebc5517eee0e9ccbf2781eecb07d
sashashreyder <sashashreiderbr@gmail.com>
2025-01-28 21:45:23 -0300

    Header nav deleted

--- todolist/src/components/Header.jsx
+++ todolist/src/components/Header.jsx
@@ -8,20 +8,14 @@ const Header = ({ onShowRegister }) => {
         <i className="fas fa-calendar-alt logo-icon" aria-hidden="true"></i>
         <h1 className="header-title">My Daily Planner</h1>
       </div>
-      <nav className="header-nav">
-        <ul className="nav-links">
-          <li><a href="#features">Features</a></li>
-          <li><a href="#about">About</a></li>
-          <li><a href="#contact">Contact</a></li>
-        </ul>
-        <button className="register-btn" onClick={onShowRegister} aria-label="Register">
+        <button className="register-btn2" onClick={onShowRegister} aria-label="Register">
           <i className="fas fa-user-plus"></i>
         </button>
-      </nav>
     </header>
   );
 };
 
 export default Header;
+
 
 
--- todolist/src/main.jsx
+++ todolist/src/main.jsx
@@ -5,7 +5,7 @@ import { GoogleOAuthProvider } from "@react-oauth/google";
 
 const clientId = import.meta.env.VITE_GOOGLE_CLIENT_ID;
 
-console.log("Google Client ID:", clientId); // Check if it's correctly loaded
+console.log("Google Client ID:", clientId); 
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <GoogleOAuthProvider clientId={clientId}>
